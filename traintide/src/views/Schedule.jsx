de59676824bbb3d4362c08c74ce148b485582dc6
rycid <rycidic@gmail.com>
2024-03-26 20:46:23 -0400

    Add Schedule component

---
+++ traintide/src/views/Schedule.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles/Schedule.scss';
+const Schedule = () => {
+    return (
+        <div>
+            <h1>Schedule</h1>
+        </div>
+    );
+}
+
+export default Schedule;
