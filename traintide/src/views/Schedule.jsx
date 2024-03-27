0c6029fc60ea38055f6ae751a73814adaff88d21
rycid <rycidic@gmail.com>
2024-03-27 03:49:56 -0400

    Add calendar functionality to Schedule component

--- traintide/src/views/Schedule.jsx
+++ traintide/src/views/Schedule.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './styles/Schedule.scss';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 const Schedule = () => {
+    const [value, onChange] = useState(new Date());
+
+    const onClickDay = (value) => {
+        console.log(value);
+    }
+
     return (
-        <div>
+        <div className="schedule">
             <h1>Schedule</h1>
+            <p>Here is your schedule for the week!</p>
+            <Calendar
+                onChange={onChange}
+                value={value}
+                onClickDay={onClickDay}
+                tileClassName={({ date, view }) => {
+                    if (date.getDay() === 0 || date.getDay() === 6) {
+                        return 'weekend';
+                    }
+                }}
+                calendarType="US"
+            />
         </div>
     );
 }
