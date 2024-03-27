0cc63e6937e6d549a85d2771e30fa26174bc6e78
rycid <rycidic@gmail.com>
2024-03-26 21:12:19 -0400

    Add new routes for profile, schedule, and settings

--- traintide/src/App.js
+++ traintide/src/App.js
@@ -26,6 +26,9 @@ const App = () => {
           <div className="content">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/schedule" element={<Schedule />} />
+              <Route path="/settings" element={<Settings />} />
               <Route path="/about" element={<About />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
