857408940ff7a42569ab64d8c1a5de22625934f5
rycid <rycidic@gmail.com>
2024-03-26 20:46:17 -0400

    Add NotFound component

---
+++ traintide/src/views/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles/NotFound.scss';
+const NotFound = () => {
+    return (
+        <div>
+            <h1>NotFound</h1>
+        </div>
+    );
+}
+
+export default NotFound;
