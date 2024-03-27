4b47da018045ad18cb689c9088cca18f8d1a485c
rycid <rycidic@gmail.com>
2024-03-26 20:05:47 -0400

    Add sidebar functionality to App component

--- traintide/src/App.js
+++ traintide/src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
+import './App.scss';
+import SideBar from './components/SideBar';
 import Header from './components/Header';
 
 const Home = () => <h1>Home</h1>;
 const About = () => <h1>About</h1>;
 const NotFound = () => <h1>404 Not Found</h1>;
 
-const App = () => (
-  <Router>
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  </Router>
-);
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <Router>
+      <div className="app-wrapper">
+        <SideBar open={sidebarOpen} setOpen={setSidebarOpen} />
+        <div className="main-content">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
