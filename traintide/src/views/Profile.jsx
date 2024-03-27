a4021b00f3d8d15a65f295b28d4ab0e9431b3a47
rycid <rycidic@gmail.com>
2024-03-27 03:02:29 -0400

    Refactor profile view component

--- traintide/src/views/Profile.jsx
+++ traintide/src/views/Profile.jsx
@@ -2,8 +2,27 @@ import React from "react";
 import './styles/Profile.scss';
 const Profile = () => {
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="profile">
+            <div className="profileTop">
+                <div className="profileBanner">
+                    <img src="https://via.placeholder.com/150" alt="banner" />
+                </div>
+                <div className="profilePic">
+                    <img src="https://via.placeholder.com/150" alt="profile" />
+                </div>
+            </div>
+            <div className="profileInfo">
+                <div className="identity">
+                    <h1 className="name">John Doe</h1>
+                    <h3 className="title">Personal Trainer</h3>
+                    <h5 className="gym">Gym Name</h5>
+                </div>
+                <div className="additional">
+                    <p className="stat age">Age: 25</p>
+                    <p className="stat location">Location: New York, NY</p>
+                    <p className="stat journey">Journey: 1 year</p>
+                </div>
+            </div>
         </div>
     );
 }
