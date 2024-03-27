00a7f858e4aaf343f5b508dad11802977ee6053f
rycid <rycidic@gmail.com>
2024-03-26 20:45:48 -0400

    Refactor routing in App.js

--- traintide/src/App.js
+++ traintide/src/App.js
@@ -5,11 +5,16 @@ import './App.scss';
 import SideBar from './components/SideBar';
 import Header from './components/Header';
 
-const Home = () => <h1>Home</h1>;
-const About = () => <h1>About</h1>;
-const NotFound = () => <h1>404 Not Found</h1>;
+//pages/views
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Schedule from './views/Schedule';
+import Settings from './views/Settings';
+import NotFound from './views/NotFound';
+import About from './views/About';
 
 const App = () => {
+  const [hideHeader, setHideHeader] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -17,12 +22,14 @@ const App = () => {
       <div className="app-wrapper">
         <SideBar open={sidebarOpen} setOpen={setSidebarOpen} />
         <div className="main-content">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Header /> 
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
