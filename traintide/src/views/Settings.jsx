03f616a544d42995f189ea3964137f188335bdd2
rycid <rycidic@gmail.com>
2024-03-26 20:46:26 -0400

    Add Settings component

---
+++ traintide/src/views/Settings.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles/Settings.scss';
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings</h1>
+        </div>
+    );
+}
+
+export default Settings;
